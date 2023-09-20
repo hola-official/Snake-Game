4033dc82023c25ea7cf58176fc61ee9cccfc23ff
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-09-20 14:59:11 +0100

    git update

--- script.js
+++ script.js
@@ -105,35 +105,70 @@ function changeDirection(event) {
   const RIGHT = 39;
   const DOWN = 40;
 
-  const goingUp = (yVelocity == -unitSize);
-  const goingDown = (yVelocity == unitSize);
-  const goingLeft = (xVelocity == unitSize);
-  const goingRight = (xVelocity == -unitSize);
+  const goingUp = yVelocity == -unitSize;
+  const goingDown = yVelocity == unitSize;
+  const goingLeft = xVelocity == -unitSize;
+  const goingRight = xVelocity == unitSize;
 
-  switch(true) {
-    case (keypressed == LEFT && !goingRight):
-      xVelocity = -unitSize
-    yVelocity = 0
-    break;
-    case (keypressed == UP && !goingDown):
-      yVelocity = -unitSize
+  switch (true) {
+    case keypressed == LEFT && !goingRight:
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+    case keypressed == UP && !goingDown:
+      yVelocity = -unitSize;
       xVelocity = 0;
       break;
-      case (keypressed == RIGHT && !goingLeft):
-      xVelocity = unitSize
-      yVelocity = 0
+    case keypressed == RIGHT && !goingLeft:
+      xVelocity = unitSize;
+      yVelocity = 0;
       break;
-      case (keypressed == DOWN && !goingUp):
-        xVelocity = 0;
-        yVelocity = unitSize
+    case keypressed == DOWN && !goingUp:
+      xVelocity = 0;
+      yVelocity = unitSize;
   }
 }
 
-function checkGameOver() {}
+function checkGameOver() {
+  switch (true) {
+    case snake[0].x < 0:
+      running = false;
+      break;
+    case snake[0].x >= gameWidth:
+      running = false;
+    case snake[0].y < 0:
+      running = false;
+      break;
+    case snake[0].y >= gameHeight:
+      running = false;
+  }
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      running = false;
+    }
+  }
+}
+function displayGameOver() {
+  ctx.font = "50px MV Boli";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER", gameWidth / 2, gameHeight / 2);
+}
 
-function displayGameOver() {}
-
-function resetGame() {}
+function resetGame() {
+  xVelocity = unitSize;
+  yVelocity = 0;
+  score = 0;
+  scoreText.textContent = score;
+  snake = [
+    { x: unitSize * 4, y: 0 },
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 },
+  ];
+  gameStart();
+}
 
 // let  x = Math.ceil(5.65)
 // console.log(x);
